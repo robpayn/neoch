6b6e6018c4b5132768fa4209d516582530314971
Robert Payn <robpayn@gmail.com>
2017-04-23 17:10:26 -0600

    Added ProcessorDoubleStore class for semantics

---
+++ src/main/java/org/payn/neoch/processors/ProcessorDoubleStore.java
@@ -0,0 +1,13 @@
+package org.payn.neoch.processors;
+
+import org.payn.chsm.processors.finitedifference.ProcessorDoubleBaseState;
+
+/**
+ * Processor for storage in a cell
+ * 
+ * @author robpayn
+ *
+ */
+public class ProcessorDoubleStore extends ProcessorDoubleBaseState {
+
+}
